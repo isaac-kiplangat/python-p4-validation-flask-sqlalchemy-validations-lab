216aa535e3e1a24685c94434e2a13f29c141b89b
5000vw <isaackiplangat1999@gmail.com>
2024-01-17 18:01:39 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy import CheckConstraint
 db = SQLAlchemy()
 
 class Author(db.Model):
@@ -12,7 +13,24 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self,key, name):
+        if not name:
+            raise ValueError('Author name cannot be empty')
+        
+        existing_author = Author.query.filter_by(name=name).first()
+        if existing_author and existing_author.id != self.id:
+            raise ValueError('Author with the same name already exists.')
 
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError('Author phone number must be exactly ten digits.')
+        return phone_number
+
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -28,7 +46,37 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
-
-
+    @validates('title')
+    def validate_name(self,key, title):
+        if not title:
+            raise ValueError('Post title is required.')
+        return title
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Post content must be at least 250 characters long.')
+        return content
+    
+    
+    @validates('summary')
+    def validate_name(self,key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError('content should be at least 250 characters ')
+        
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError('Post category must be either Fiction or Non-Fiction.')
+        return category
+    
+    __table_args__ = (
+        CheckConstraint(
+            category.in_(['Fiction', 'Non-Fiction']),
+            name='valid_category'
+        ),
+    )
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
